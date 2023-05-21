407f723bfae2214f552e4b41c796b659f6494cfd
Akhil Eaga <akhil.bitsian2013@gmail.com>
2023-05-22 00:48:31 +0800

    Invalidate previous API key and used env variables for secure API key management

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "df42c7182e164954847b3a0b28e9a86d",
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
