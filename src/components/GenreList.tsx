8375019a0201bf0e787aea89ca45b8e4e059b8ec
Akhil Eaga <akhil.bitsian2013@gmail.com>
2023-05-23 22:13:43 +0800

    Showing a list item skeleton while genres are loading

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,25 @@
 import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import GenreListItemSkeleton from "./GenreListItemSkeleton";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
+  const skeletons = Array(20)
+    .fill(1)
+    .map((element, index) => index + 1);
 
   if (error) return null; // no need to show so many errors
-  if (isLoading) return <Spinner />;
+  //   if (isLoading) return <Spinner />;
 
   return (
     <List>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <ListItem paddingY="10px">
+            <GenreListItemSkeleton key={skeleton} />
+          </ListItem>
+        ))}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
---
+++ src/components/GenreListItemSkeleton.tsx
@@ -0,0 +1,7 @@
+import { Box, HStack, Skeleton, SkeletonCircle } from "@chakra-ui/react";
+
+const GenreListItemSkeleton = () => {
+  return <Skeleton height="15px" />;
+};
+
+export default GenreListItemSkeleton;
