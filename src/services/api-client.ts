9263f442362d8bc248f122b2c899ae542d19cac4
Akhil Eaga <akhil.bitsian2013@gmail.com>
2023-05-25 01:34:56 +0800

    CORS fix from vercel docs

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -6,7 +6,10 @@ export default axios.create({
     key: import.meta.env.VITE_RAWG_API_KEY,
   },
   headers: {
+    "Access-Control-Allow-Credentials": "true",
     "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+    "Access-Control-Allow-Headers":
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
   },
-  withCredentials: false,
 });
