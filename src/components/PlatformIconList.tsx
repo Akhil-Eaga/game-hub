7a5934c5370b63a53318c18cee90de1ccb92c006
Akhil Eaga <akhil.bitsian2013@gmail.com>
2023-06-06 01:37:43 +0800

    Refactored entity interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,17 +1,17 @@
+import { HStack, Icon } from "@chakra-ui/react";
 import { IconType } from "react-icons";
+import { BsGlobe } from "react-icons/bs";
 import {
-  FaWindows,
-  FaPlaystation,
-  FaXbox,
-  FaApple,
   FaAndroid,
+  FaApple,
   FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
 } from "react-icons/fa";
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
-import { BsGlobe } from "react-icons/bs";
-import { HStack, Icon } from "@chakra-ui/react";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 interface Props {
   platforms: Platform[];
 }
