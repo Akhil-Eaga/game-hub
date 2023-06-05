a68dca08c292d0a9c272dada420bc7c853a8f14c
Akhil Eaga <akhil.bitsian2013@gmail.com>
2023-06-06 01:31:00 +0800

    Clicking logo and performing search takes user to homepage

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,6 +1,7 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 import useGameQueryStore from "../store";
 
 const SearchInput = () => {
@@ -8,12 +9,16 @@ const SearchInput = () => {
   // using a selector this way ensures this component does not re-render
   // when something other than the selected store property changes
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (ref.current) setSearchText(ref.current.value);
+        if (ref.current) {
+          setSearchText(ref.current.value);
+          navigate("/");
+        }
       }}
     >
       <InputGroup>
